6b2a8c415b5e9159907ffd222be9386d35b117f5
Jan <jan280808@gmail.com>
2024-08-11 01:54:18 +0200

    add logger to... log things

--- src/main/java/de/jan/skyblock/SkyBlock.java
+++ src/main/java/de/jan/skyblock/SkyBlock.java
@@ -1,12 +1,15 @@
 package de.jan.skyblock;
 
 import de.jan.skyblock.command.IslandCommand;
+import de.jan.skyblock.command.WorldCommand;
 import de.jan.skyblock.island.IslandManager;
 import de.jan.skyblock.player.PlayerManager;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
@@ -14,6 +17,7 @@ import java.util.Objects;
 public final class SkyBlock extends JavaPlugin {
 
     public static SkyBlock instance;
+    public static final Logger Logger = LoggerFactory.getLogger("SkyBlock");
     private PlayerManager playerManager;
     private IslandManager islandManager;
 
@@ -38,5 +42,6 @@ public final class SkyBlock extends JavaPlugin {
 
     private void registerCommands() {
         Objects.requireNonNull(getCommand("island")).setExecutor(new IslandCommand(islandManager));
+        Objects.requireNonNull(getCommand("world")).setExecutor(new WorldCommand());
     }
 }
