0de9e9f4f5b6d190a1639b34d3512ab030c8a9f4
Jan <jan280808@gmail.com>
2024-08-11 23:22:08 +0200

    add maxRadius to define the maximum size of an island

--- src/main/java/de/jan/skyblock/island/Island.java
+++ src/main/java/de/jan/skyblock/island/Island.java
@@ -1,9 +1,7 @@
 package de.jan.skyblock.island;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.Location;
-
 import java.util.UUID;
 
 @Getter
@@ -12,11 +10,13 @@ public class Island {
     private final UUID owner;
     private final Location center;
     private final int id;
+    private int maxRadius;
 
     public Island(UUID owner, int id, Location center) {
         this.owner = owner;
         this.id = id;
         this.center = center;
+        this.maxRadius = 50;
     }
 
     public void teleport() {
