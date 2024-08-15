58957c8bfd7de8c0f2e7c80ffd88f3b15d2e0844
Jan <jan280808@gmail.com>
2024-08-15 22:10:18 +0200

    add boolean to decide which command should displayed in autotab and which not

--- src/main/java/de/jan/skyblock/command/islandCommands/subcommand/SubCommands.java
+++ src/main/java/de/jan/skyblock/command/islandCommands/subcommand/SubCommands.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 
 @Getter
 public enum SubCommands {
-    CREATE_ISLAND(new CreateIslandCommand(), "create"),
-    INFO(new InfoCommand(), "info");
+    CREATE_ISLAND(new CreateIslandCommand(), "create", true),
+    INFO(new InfoCommand(), "info", true),
+    DEBUG(new DebugCommand(), "debug", false);
 
     private final IslandCommands islandCommands;
     private final String subCommand;
+    private final boolean displayCommand;
 
-    SubCommands(IslandCommands islandCommands, String subCommand) {
+    SubCommands(IslandCommands islandCommands, String subCommand, Boolean displayCommand) {
         this.islandCommands = islandCommands;
         this.subCommand = subCommand;
+        this.displayCommand = displayCommand;
     }
 }
