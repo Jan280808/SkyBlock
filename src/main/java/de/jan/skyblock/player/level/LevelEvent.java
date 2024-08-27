1bc2c68bcb663c3cc7727b8e69905ae33cb7b93f
Jan <jan280808@gmail.com>
2024-08-27 23:29:21 +0200

    add more items which xp is generated during mining

--- src/main/java/de/jan/skyblock/player/level/LevelEvent.java
+++ src/main/java/de/jan/skyblock/player/level/LevelEvent.java
@@ -2,11 +2,12 @@ package de.jan.skyblock.player.level;
 
 import de.jan.skyblock.player.PlayerManager;
 import de.jan.skyblock.player.SkyPlayer;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+
+import java.util.Arrays;
 
 public class LevelEvent implements Listener {
 
@@ -20,7 +21,7 @@ public class LevelEvent implements Listener {
     public void onBreak(BlockBreakEvent event) {
         SkyPlayer skyPlayer = this.playerManager.getSkyPlayer(event.getPlayer().getUniqueId());
         Block block = event.getBlock();
-        if(!block.getType().equals(Material.COBBLESTONE)) return;
-        skyPlayer.getMiningLevel().addXP(0.01);
+        if(!skyPlayer.isOnIsland()) return;
+        Arrays.stream(MiningLevel.MiningBlocks.values()).filter(miningBlocks -> block.getType().equals(miningBlocks.getMaterial())).forEach(miningBlocks -> skyPlayer.getMiningLevel().addXP(miningBlocks.getXp()));
     }
 }
--- src/main/java/de/jan/skyblock/player/level/MiningLevel.java
+++ src/main/java/de/jan/skyblock/player/level/MiningLevel.java
@@ -2,23 +2,43 @@ package de.jan.skyblock.player.level;
 
 import de.jan.skyblock.builder.ItemBuilder;
 import de.jan.skyblock.component.ComponentSerializer;
+import de.jan.skyblock.player.SkyPlayer;
+import de.jan.skyblock.sound.SoundManager;
+import de.jan.skyblock.sound.Sounds;
+import lombok.Getter;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class MiningLevel implements Level {
 
+    private final SkyPlayer skyPlayer;
+
     private double currentXP = 0.00;
     private int currentLevel = 0;
 
+    public MiningLevel(SkyPlayer skyPlayer) {
+        this.skyPlayer = skyPlayer;
+    }
+
     @Override
     public Component displayName() {
-        return ComponentSerializer.deserialize("<gold>Mining-Level");
+        return ComponentSerializer.deserialize("<gold>Mining-Level ");
     }
 
     @Override
     public ItemStack displayItem() {
-        return new ItemBuilder(Material.GOLDEN_PICKAXE).setDisplayName(displayName()).setLore("CurrentLevel: " + currentLevel, "currentXP: " + currentXP).build();
+        return new ItemBuilder(Material.GOLDEN_PICKAXE).setDisplayName(displayName()).setLore("<gray>CurrentLevel: " + currentLevel, "<gray>neededXP: " + nextLevelXP() + " - currentXP: " + currentXP()).build();
+    }
+
+    @Override
+    public double baseXP() {
+        return 100.0;
+    }
+
+    @Override
+    public double multiplier() {
+        return 1.5;
     }
 
     @Override
@@ -27,17 +47,55 @@ public class MiningLevel implements Level {
     }
 
     @Override
-    public double currentXP() {
-        return currentXP;
+    public String currentXP() {
+        return String.format("%.2f", currentXP);
     }
 
     @Override
     public void addXP(double amount) {
-        currentXP = currentXP + amount;
+        currentXP += amount;
+        skyPlayer.getPlayer().sendActionBar(displayName().append(ComponentSerializer.deserialize("<gray>" + currentXP() + "/" + nextLevelXP())));
+        while (currentXP >= nextLevelXP()) {
+            currentXP -= nextLevelXP();
+            levelUP();
+        }
+    }
+
+    @Override
+    public double nextLevelXP() {
+        return baseXP() * Math.pow(multiplier(), currentLevel);
     }
 
     @Override
     public void levelUP() {
         currentLevel = currentLevel +1;
+        SoundManager.playSound(Sounds.SUCCESSES, skyPlayer.getPlayer());
+        skyPlayer.getPlayer().sendActionBar(displayName().append(ComponentSerializer.deserialize("<gold>LevelUP aktuelles MiningLevel: " + currentLevel())));
+    }
+
+    @Override
+    public boolean hasLevel(int requiredLevel) {
+        return currentLevel >= requiredLevel;
+    }
+
+    @Getter
+    public enum MiningBlocks {
+        COBBLESTONE(Material.COBBLESTONE, 0.01),
+        DEEPSLATE(Material.DEEPSLATE, 0.02), 
+        ORE_COAL(Material.COAL_ORE, 10),
+        ORE_IRON(Material.IRON_ORE, 10),
+        ORE_GOLD(Material.GOLD_ORE, 10),
+        ORE_REDSTONE(Material.REDSTONE_ORE, 10),
+        ORE_LAPIS(Material.LAPIS_ORE, 10),
+        ORE_DIAMOND(Material.DIAMOND_ORE, 10),
+        ORE_EMERALD(Material.EMERALD_ORE, 10);
+
+        private final Material material;
+        private final double xp;
+
+        MiningBlocks(Material material, double xp) {
+            this.material = material;
+            this.xp = xp;
+        }
     }
 }
